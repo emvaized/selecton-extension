9778e82db35c7ddb9997ae6e80aff34eee4a9424
emvaized <maximtsyba@gmail.com>
2024-08-14 20:05:48 +0200

    Remove old icons

--- src/functions/background.js
+++ src/functions/background.js
@@ -69,7 +69,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
 function displayNotification(title, message, link, image) {
     chrome.notifications.create({
         "type": "basic",
-        "iconUrl": image ?? "../../icons/logo-96.png",
+        "iconUrl": image ?? "./icons/logo-new.png",
         "title": title,
         "message": message,
     }, function (notificationId) {
