6519a1ba7bc3acee7ec3f9f15441f17c85f576f0
emvaized <maximtsyba@gmail.com>
2024-09-20 04:49:28 +0200

    Fix broken contenteditable detection

--- src/index.js
+++ src/index.js
@@ -324,7 +324,7 @@ function initMouseListeners() {
           target.type == 'email' || 
           target.type == 'search' || 
           target.type == 'text'
-      )) ||  target.tagName === "TEXTAREA" || target.contenteditable;
+      )) ||  target.tagName === "TEXTAREA" || target.hasAttribute('contenteditable');
 
     if (isTextFieldFocused && configs.addActionButtonsForTextFields) {
       /// Special handling for Firefox 
