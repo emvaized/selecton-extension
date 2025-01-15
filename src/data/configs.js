f4886222ccc13766b6b988fff29f7df9c42058cd
Max <maximtsyba@gmail.com>
2025-01-15 23:55:55 +0100

    Disable recreating tooltip on scroll and hide when cursor moves away by default

--- src/data/configs.js
+++ src/data/configs.js
@@ -63,7 +63,7 @@ const configs = {
     'secondaryTooltipLayout': 'verticalLayout', /// Possible values: 'horizontalLayout', 'verticalLayout'
     'liveTranslation': true,
     // 'reverseTooltipButtonsOrder': false,
-    'recreateTooltipAfterScroll': true,
+    'recreateTooltipAfterScroll': false,
     'applyConfigsImmediately': false,
     'invertColorOnDarkWebsite': true,
     'addPasteOnlyEmptyField': true,
@@ -91,7 +91,7 @@ const configs = {
     'verticalLayoutTooltip': false,
     'addButtonToCopyLinkToText': true,
     'addCalendarButton': true,
-    'hideTooltipWhenCursorMovesAway': true,
+    'hideTooltipWhenCursorMovesAway': false,
     'showInfoPanel': true,
     'maxTooltipButtonsToShow': 3,
     'maxMarkerPagesToStore': 10,
